38f48129e8a5d377022e20c2f59c5b5ff6919a8c
RomainBasly <98778180+RomainBasly@users.noreply.github.com>
2022-02-11 16:33:57 +0100

    Voici le résultat final

--- Exercice semaine 2/html_template/js/script.js
+++ Exercice semaine 2/html_template/js/script.js
@@ -1,7 +1,26 @@
 const nom = prompt("Quel est ton petit nom?");
-const presentation = `bonjour, ${nom}`;
-const h2 = document.createElement('h2');
-document.body.append(h2);
-const h1 = document.getElementById('#h1');
+const presentation = `${nom}`;
 
-h2.textContent = presentation;
+function askName() {
+    const h2 = document.createElement('h2');
+    document.body.append(h2);
+    const h1 = document.getElementById('#h1');
+    document.body.innerHTML += `<h2> Coucou! ${presentation} </h2>`;
+}
+// h2.textContent = presentation;
+
+const dateNaissance = prompt("Et plus précisément, quelle est ta date de naissance complète?", "YYYY-MM-JJ");
+
+function askExactAge() {
+    const dateNaissanceReelle = new Date(dateNaissance);
+    const aujourdHui = new Date();
+    const age = aujourdHui - dateNaissanceReelle;
+    const ageReel = Math.floor(age / (1000 * 365 * 24 * 3600));
+
+    const h3 = document.createElement('h3');
+    document.body.append(h3);
+    document.body.innerHTML += `<h3>Vous avez ${ageReel} ans.</h3>`;
+}
+
+askName(nom);
+askExactAge(dateNaissance);

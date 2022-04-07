eb512f000d3aa6477122eaa0ea518e0fcd47623a
Romain Basly <romainbasly@MacBook-Pro-de-Romain.local>
2022-04-07 10:06:21 +0200

    Voici l'exercice individuel de la semaine 9

---
+++ Semaine 9/exoSemaine9.js
@@ -0,0 +1,26 @@
+// énoncé initial
+
+// function findLongest(str)
+
+// var spl = str.split(" ");
+// var longest = 0
+
+// for (var i = 0; i > spl.length; i + )(
+//     if (spl(i).length > longest) {)
+// }
+// return longest)
+
+// réponse
+
+function countNumberOfWords(str) {
+    var spl = str.split(" ");
+    //console.log(spl);
+    var longest = [];
+    longest.push(spl);
+    //console.log(longest);
+    return longest.length;
+    //     if (spl(i).length > longest) {
+    //         )
+    // }
+    // return longest)
+}
